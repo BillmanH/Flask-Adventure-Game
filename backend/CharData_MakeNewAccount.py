76da2fd456e5820e842360f6d6f2ccb5c3cd8aac
William Harding <william.jeffrey.harding@gmail.com>
2017-10-18 16:12:25 -0700

    checkIfAccountExists is working propperly

--- backend/CharData_MakeNewAccount.py
+++ backend/CharData_MakeNewAccount.py
@@ -5,12 +5,20 @@ import yaml
 conn = S3Connection()
 mybucket = conn.get_bucket('flaskgame')
 
-def checkIfAccountExists(mybucket,key="william.jeffrey.harding@gmail.comcharData.json"):
+def checkIfAccountExists(key="william.jeffrey.harding@gmail.comcharData.json"):
 	folder = 'chars'
 	objs = list(mybucket.objects.filter(Prefix=folder + key))
 	if len(objs) > 0 and objs[0].key == key:
 		return True
 	return False
+
+def checkIfAccountExists(id="william.jeffrey.harding@gmail.comcharData.json"):
+	myKey = mybucket.get_key("chars/" + id)
+	if myKey==None:
+		return False
+	else:
+		return True
+
 
 def saveNewCharacterData(formData):
 	charData = formData
