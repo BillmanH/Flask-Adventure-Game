7d24ef7ddf47eb66c53c4403ac5048a1522eee37
William Harding <william.jeffrey.harding@gmail.com>
2017-10-18 08:54:01 -0700

    Last commit didnt' take?

--- backend/CharData_MakeNewAccount.py
+++ backend/CharData_MakeNewAccount.py
@@ -3,11 +3,17 @@ import boto
 import yaml
 
 conn = S3Connection()
+mybucket = conn.get_bucket('flaskgame')
 
-def checkIfAccountExists():
-	return True
+def checkIfAccountExists(mybucket,key="william.jeffrey.harding@gmail.comcharData.json"):
+	objs = list(mybucket.objects.filter(Prefix=key))
+	if len(objs) > 0 and objs[0].key == key:
+		return True
+	return False
 
 def saveNewCharacterData(formData):
 	charData = formData
 	charData['account info']['email'] = formData['email']
+	charData['account info']['password'] = "password"
+
 	
