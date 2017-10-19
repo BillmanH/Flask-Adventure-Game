65a133880704e4bb3cd52372dee9fbf0c37a758f
William Harding <william.jeffrey.harding@gmail.com>
2017-10-19 08:46:54 -0700

    added write function,not tested

--- backend/CharData_MakeNewAccount.py
+++ backend/CharData_MakeNewAccount.py
@@ -5,12 +5,6 @@ import yaml
 conn = S3Connection()
 mybucket = conn.get_bucket('flaskgame')
 
-def checkIfAccountExists(key="william.jeffrey.harding@gmail.comcharData.json"):
-	folder = 'chars'
-	objs = list(mybucket.objects.filter(Prefix=folder + key))
-	if len(objs) > 0 and objs[0].key == key:
-		return True
-	return False
 
 def checkIfAccountExists(id="william.jeffrey.harding@gmail.comcharData.json"):
 	myKey = mybucket.get_key("chars/" + id)
@@ -24,5 +18,19 @@ def saveNewCharacterData(formData):
 	charData = formData
 	charData['account info']['email'] = formData['email']
 	charData['account info']['password'] = "password"
+	charData['id'] = charData[charData['account info']
+	if(checkIfAccountExists(charData['id'])):
+		characterAlreadyExists()
+	else:
+		myKey = mybucket.get_key("chars/" + charData['id'])
+		myKey.set_contents_from_string(str(charData))
+
+def sendNewCharacterInfo(charData):
+	pass
+
+def characterAlreadyExists(charData):
+	pass
+
+
 
 	
