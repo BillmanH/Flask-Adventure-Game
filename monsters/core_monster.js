96969a9cf351d246651777c24831ce35e4a53865
William Harding <william.jeffrey.harding@gmail.com>
2018-08-12 18:11:59 -0700

    monsters now move around in a very brownian way

--- monsters/core_monster.js
+++ monsters/core_monster.js
@@ -1,2 +1,4 @@
-console.log("This posted")
-
+d3.selectAll(".monster")
+	.transition()
+		.attr("cx",function(d, i) { return (parseInt(d3.select(this).attr("cx")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})	
+		.attr("cy",function(d, i) { return (parseInt(d3.select(this).attr("cy")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
