3e32b95ebc6453a7258f41cb01ee1d543c57327a
William Harding <william.jeffrey.harding@gmail.com>
2018-08-26 07:44:06 -0700

    added monster spotting character. move_towards_char stillnot working like I would like

--- monsters/core_monster.js
+++ monsters/core_monster.js
@@ -1,4 +1,19 @@
+//this is the code thatmonsters do on thier turn. 
+//   Turn order is set in the activity_map_boarder.html
+//   in the same places as the svgClick function
+
 d3.selectAll(".monster")
-	.transition()
-		.attr("cx",function(d, i) { return (parseInt(d3.select(this).attr("cx")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})	
-		.attr("cy",function(d, i) { return (parseInt(d3.select(this).attr("cy")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
+	.each(function(d,i) {
+		if(get_dist_to_char(d3.select(this).attr("cx"),d3.select(this).attr("cy"))<=15){ //TODO replace arbitrary value with monster's perception
+			objectAlerts("#character",d3.select(this).attr("id")+" Has spotted " + charData["name"]);
+			nmc = move_towards_char(d3.select(this).attr("cx"),d3.select(this).attr("cy"),d3.select(this).attr("move"))
+			d3.select(this).transition()
+				.attr("cx",nmc[0])
+				.attr("cy",nmc[1])
+		}
+		else {
+			d3.select(this).transition()
+				.attr("cx",function(d, i) { return (parseInt(d3.select(this).attr("cx")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
+				.attr("cy",function(d, i) { return (parseInt(d3.select(this).attr("cy")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
+		}
+	})
