ff62e2030ba7607d95bfffb0e208fd41ab7cfc06
William Harding <william.jeffrey.harding@gmail.com>
2018-10-22 19:37:57 -0700

    first attacking function

--- monsters/core_monster.js
+++ monsters/core_monster.js
@@ -4,11 +4,18 @@
 
 d3.selectAll(".monster")
 	.each(function(d,i) {
+		// the perception is in the metadata for monsters. if the character moves within the range of perception, the character is noticed. 
 		if(get_dist_to_char(d3.select(this).attr("cx"),d3.select(this).attr("cy"))<=mapData["monsters"]["meta"]["perception"]){
+			// detectsPlayer defaults to False
 			if (d3.select(this).classed("detectsPlayer")==false){
 				objectAlerts("#character",d3.select(this).attr("id")+" Has spotted " + charData["name"]);
 				d3.select(this).classed("detectsPlayer",true);
 			}
+			// if the character is close enough to attack, it attacks.
+			if(get_dist_to_char(d3.select(this).attr("cx"),d3.select(this).attr("cy"))<=charData["size"]){
+				// TODO : break the attack out to it's own function.
+				objectAlerts("#character",d3.select(this).attr("id")+" struck at the player ");
+			}	
 			nmc = move_towards_obj(Math.round(d3.select(this).attr("cx")),
 						Math.round(d3.select(this).attr("cy")),
 						char_x,
@@ -18,6 +25,7 @@ d3.selectAll(".monster")
 				.attr("cx",nmc[0])
 				.attr("cy",nmc[1])
 		}
+		// else if the character is not detected the moster does whateverthey were going to do
 		else {
 			d3.select(this).transition()
 				.attr("cx",function(d, i) { return (parseInt(d3.select(this).attr("cx")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
