9b8681381c48cca89b10957330a1a0191ffb7618
William Harding <william.jeffrey.harding@gmail.com>
2018-11-17 08:02:21 -0800

    character Death complete!

--- monsters/monster_attack.js
+++ monsters/monster_attack.js
@@ -4,4 +4,7 @@ var damage = randBetween(0,mapData["monsters"]["meta"]["damage"])*-1;
 objectAlerts("#"+d3.select(this).attr("id"),d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"])
 objectAlerts("#character", damage.toString())
 charData["health"] = charData["health"]+damage
-if(charData["health"]<0){objectAlerts("#character",charData["name"] + " has died")} 
+if(charData["health"]<0){
+	objectAlerts("#character",charData["name"] + " has died")
+	charDeath()
+} 
