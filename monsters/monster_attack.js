d1b4dd82af899beb8302a4c5b7fbcdc98cc02d2c
William Harding <william.jeffrey.harding@gmail.com>
2018-11-16 17:24:48 -0800

    character can die

--- monsters/monster_attack.js
+++ monsters/monster_attack.js
@@ -1,3 +1,7 @@
 // This is where the monster attack happens
-objectAlerts("#character",d3.select(this).attr("name")+" struck at the player ");
-
+var damage = randBetween(0,mapData["monsters"]["meta"]["damage"])*-1;
+	damage = Math.round(damage*100)/100
+objectAlerts("#"+d3.select(this).attr("id"),d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"])
+objectAlerts("#character", damage.toString())
+charData["health"] = charData["health"]+damage
+if(charData["health"]<0){objectAlerts("#character",charData["name"] + " has died")} 
