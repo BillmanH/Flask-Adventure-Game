6098520bced03eef9bf6cd4cadd7dc686b0d775b
William Harding <william.jeffrey.harding@gmail.com>
2018-12-09 17:47:23 -0800

    improved metadata on save/load

--- charModifiers/charattack.js
+++ charModifiers/charattack.js
@@ -1,2 +1,9 @@
-function charattack(){}
+function charattack(target){
+	//step one: deterimine damage
+		
+
+	//step two: Apply damage to character
+	d3.select(target)
+	
+}
 
--- monsters/monster_attack.js
+++ monsters/monster_attack.js
@@ -1,10 +1,17 @@
 // This is where the monster attack happens
 var damage = randBetween(0,mapData["monsters"]["meta"]["damage"])*-1;
 	damage = Math.round(damage*100)/100
+
+//send an alert to the screen about the attack
 objectAlerts("#"+d3.select(this).attr("id"),d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"])
 objectAlerts("#character", damage.toString())
+//subtract the damage from the health of the player
 charData["health"] = charData["health"]+damage
+
+//action if the characterhas died
 if(charData["health"]<0){
 	objectAlerts("#character",charData["name"] + " has died")
 	charDeath()
-} 
+} else {charAttack()} //the character automatically attacks back if able
+
+ 
