5bbc9a9bb8a9299588180de31d3f76c5d5761f2a
William Harding <william.jeffrey.harding@gmail.com>
2018-12-14 08:19:32 -0800

    way better text display, much easeir to read

--- monsters/monster_attack.js
+++ monsters/monster_attack.js
@@ -3,8 +3,7 @@ var damage = randBetween(0,mapData["monsters"]["meta"]["damage"])*-1;
 	damage = Math.round(damage*100)/100
 
 //send an alert to the screen about the attack
-objectAlerts("#"+d3.select(this).attr("id"),d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"])
-objectAlerts("#character", damage.toString())
+objectAlerts("#"+d3.select(this).attr("id"),damage.toString() + ": " + d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"])
 //subtract the damage from the health of the player
 charData["health"] = charData["health"]+damage
 
