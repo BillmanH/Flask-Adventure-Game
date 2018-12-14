d4ae79036e9e64b7c9077a37427bcbe5aabf0a60
William Harding <william.jeffrey.harding@gmail.com>
2018-12-13 16:37:50 -0800

    added checks to see if character has an equipped weapon

--- charModifiers/charattack.js
+++ charModifiers/charattack.js
@@ -1,8 +1,10 @@
 function charattack(target){
 	//step one: deterimine damage
-	console.log('whack')
-		
-
+	if('equiped' in charData){ //first a check to see if they have a character that has the 'equiped' feature
+		if (charData["equiped"]["left hand"].damage == undefined)&(charData["equiped"]["right hand"].damage == undefined){
+			objectAlerts('#character',charData['name']+'has no weapon')
+		}
+	}
 	//step two: Apply damage to character
 	d3.select(target)
 	
