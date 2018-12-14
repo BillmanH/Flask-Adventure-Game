8a628a68bfa602d6fa4d453483eb785cd42f08ea
William Harding <william.jeffrey.harding@gmail.com>
2018-12-14 08:10:32 -0800

    two step transition in objectAlerts() makes it much more legible

--- charModifiers/charattack.js
+++ charModifiers/charattack.js
@@ -1,8 +1,15 @@
 function charattack(target){
-	//step one: deterimine damage
+	// default is that the character attacks with fists
+	var attacksWith = 'fists'
+	var charDamage = [1,2]
+	// checktoSee if a character has equipped items that do damage
 	if('equiped' in charData){ //first a check to see if they have a character that has the 'equiped' feature
-		if (charData["equiped"]["left hand"].damage == undefined)&(charData["equiped"]["right hand"].damage == undefined){
+		if ((charData["equiped"]["left hand"].damage == undefined)&&(charData["equiped"]["right hand"].damage == undefined)){
 			objectAlerts('#character',charData['name']+'has no weapon')
+		} else {
+			if(charData["equiped"]["left hand"].damage == undefined){
+
+			}
 		}
 	}
 	//step two: Apply damage to character
