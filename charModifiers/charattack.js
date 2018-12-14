75a6da7268d86a0add7769229c39b35cd2899c5f
William Harding <william.jeffrey.harding@gmail.com>
2018-12-14 08:46:41 -0800

    character damage with left hand weapon is working

--- charModifiers/charattack.js
+++ charModifiers/charattack.js
@@ -7,9 +7,12 @@ function charattack(target){
 		if ((charData["equiped"]["left hand"].damage == undefined)&&(charData["equiped"]["right hand"].damage == undefined)){
 			objectAlerts('#character',charData['name']+'has no weapon')
 		} else {
-			if(charData["equiped"]["left hand"].damage == undefined){
-
+			if(charData["equiped"]["left hand"].damage != undefined){
+				damage=randBetween(charData["equiped"]["left hand"].damage[0],charData["equiped"]["left hand"].damage[1])
+				damage = (Math.round(damage*100)/100)*-1
+				objectAlerts('#character',damage.toString()+": " + charData['name']+' attacks with '+charData["equiped"]["left hand"].name)
 			}
+			
 		}
 	}
 	//step two: Apply damage to character
