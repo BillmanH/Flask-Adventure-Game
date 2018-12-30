4f6ffb98ea55e56a4f9e35f04bb06c27f97e9f22
William Harding <william.jeffrey.harding@gmail.com>
2018-12-30 13:35:59 -0800

    fixed some issues with not having a weapon equiped

--- charModifiers/charattack.js
+++ charModifiers/charattack.js
@@ -4,19 +4,27 @@ function charattack(target){
 	var charDamage = [1,2]
 	// checktoSee if a character has equipped items that do damage
 	if('equiped' in charData){ //first a check to see if they have a character that has the 'equiped' feature
-		if ((charData["equiped"]["left hand"].damage == undefined)&&(charData["equiped"]["right hand"].damage == undefined)){
+		if(charData["equiped"]["strong hand"] == undefined)
+			{ //check that either hand has been defined.
+			charData["equiped"]["strong hand"]={}}
+		if((charData["equiped"]["off hand"] == undefined))
+			{charData["equiped"]["off hand"]={}}
+		if ((charData["equiped"]["strong hand"].damage == undefined)&&(charData["equiped"]["off hand"].damage == undefined)){
 			objectAlerts('#character',charData['name']+'has no weapon')
 		} else {
-			if(charData["equiped"]["left hand"].damage != undefined){
-				damage=randBetween(charData["equiped"]["left hand"].damage[0],charData["equiped"]["left hand"].damage[1])
+			if(charData["equiped"]["strong hand"].damage != undefined){
+				damage=randBetween(charData["equiped"]["strong hand"].damage[0],charData["equiped"]["strong hand"].damage[1])
 				damage = (Math.round(damage*100)/100)*-1
-				objectAlerts('#character',damage.toString()+": " + charData['name']+' attacks with '+charData["equiped"]["left hand"].name)
+				objectAlerts('#character',
+						damage.toString()+": " + charData['name']+' attacks with '+charData["equiped"]["strong hand"].name,
+						color="#0000ff")
 			}
 			
 		}
 	}
 	//step two: Apply damage to character
-	d3.select(target)
 	
 }
+
+function damageMonster(){}
 
