934043f1af42a1e55f52eabb8ccbe5795133561e
William Harding <william.jeffrey.harding@gmail.com>
2018-12-30 12:45:05 -0800

    monstors attack alert is in red

--- monsters/monster_attack.js
+++ monsters/monster_attack.js
@@ -3,7 +3,10 @@ var damage = randBetween(0,mapData["monsters"]["meta"]["damage"])*-1;
 	damage = Math.round(damage*100)/100
 
 //send an alert to the screen about the attack
-objectAlerts("#"+d3.select(this).attr("id"),damage.toString() + ": " + d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"])
+objectAlerts("#"+d3.select(this).attr("id"),
+		damage.toString() + ": " + d3.select(this).attr("name")+" "+mapData["monsters"]["meta"]["attackType"],
+		color="#8b0000")
+
 //subtract the damage from the health of the player
 charData["health"] = charData["health"]+damage
 
