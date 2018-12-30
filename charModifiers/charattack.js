8e2fe5cffb829f9aeb02bac6a995d0ca9b45001d
William Harding <william.jeffrey.harding@gmail.com>
2018-12-30 13:50:43 -0800

    unarmed attacks now working

--- charModifiers/charattack.js
+++ charModifiers/charattack.js
@@ -1,7 +1,12 @@
+function calculateDamage(damageRange=[0,1]){
+	
+}
+
 function charattack(target){
 	// default is that the character attacks with fists
 	var attacksWith = 'fists'
 	var charDamage = [1,2]
+	var charColor = "#0000ff"
 	// checktoSee if a character has equipped items that do damage
 	if('equiped' in charData){ //first a check to see if they have a character that has the 'equiped' feature
 		if(charData["equiped"]["strong hand"] == undefined)
@@ -10,14 +15,19 @@ function charattack(target){
 		if((charData["equiped"]["off hand"] == undefined))
 			{charData["equiped"]["off hand"]={}}
 		if ((charData["equiped"]["strong hand"].damage == undefined)&&(charData["equiped"]["off hand"].damage == undefined)){
-			objectAlerts('#character',charData['name']+'has no weapon')
-		} else {
+			objectAlerts('#character',charData['name']+' has no weapon')
+			damage=randBetween(charDamage[0],charDamage[1])
+			damage = (Math.round(damage*100)/100)*-1
+			objectAlerts('#character',
+				damage.toString()+": " + charData['name']+' attacks with '+ attacksWith,
+				color=charColor)
+		} else { //strong hand attack
 			if(charData["equiped"]["strong hand"].damage != undefined){
 				damage=randBetween(charData["equiped"]["strong hand"].damage[0],charData["equiped"]["strong hand"].damage[1])
 				damage = (Math.round(damage*100)/100)*-1
 				objectAlerts('#character',
 						damage.toString()+": " + charData['name']+' attacks with '+charData["equiped"]["strong hand"].name,
-						color="#0000ff")
+						color=charColor)
 			}
 			
 		}
@@ -26,5 +36,4 @@ function charattack(target){
 	
 }
 
-function damageMonster(){}
 
