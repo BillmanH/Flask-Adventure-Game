aaf5e9c20883d76762d336789b07ec147d7652eb
William Harding <william.jeffrey.harding@gmail.com>
2019-01-26 23:13:36 -0800

    fixed an issue where the element would error if there is a space in the monster name

---
+++ globals/qa.js
@@ -0,0 +1,3 @@
+function qaID(idText){
+	return idText.replace(" ", "-");
+}
--- monsters/core_monster.js
+++ monsters/core_monster.js
@@ -26,6 +26,7 @@ d3.selectAll(".monster")
 		}
 		// else if the character is not detected the moster does whateverthey were going to do
 		else {
+			
 			d3.select(this).transition()
 				.attr("cx",function(d, i) { return (parseInt(d3.select(this).attr("cx")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
 				.attr("cy",function(d, i) { return (parseInt(d3.select(this).attr("cy")))+(coordshifter(parseInt(d3.select(this).attr("move"))))})
